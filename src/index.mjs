70cdd6e2196ded6141318db839c055cb7bfdbf04
André Timermann <andre@timermann.com.br>
2023-07-08 00:33:03 -0300

    novo sleep

--- src/index.mjs
+++ src/index.mjs
@@ -10,10 +10,6 @@ import { fileURLToPath } from 'url'
 import { dirname } from 'path'
 import { readFile } from 'fs/promises'
 
-// -----------------------------------------------------------------
-// Inquirer Prompt
-// -----------------------------------------------------------------
-
 export function __dirname (importMetaURL) {
   return dirname(fileURLToPath(importMetaURL))
 }
@@ -33,4 +29,21 @@ export async function parseCommand (commandString) {
   const args = parts.slice(1)
 
   return [command, args]
+}
+
+/**
+ * Creates a delay in the execution of the JavaScript code.
+ *
+ * @param {number} ms - The amount of time, in milliseconds, to delay the execution of the subsequent code.
+ *
+ * @returns {Promise} - A Promise that resolves after the specified delay.
+ *
+ * @example
+ * // Print 'Hello', wait for 2 seconds, then print 'World'.
+ * console.log('Hello');
+ * await sleep(2000);
+ * console.log('World');
+ */
+export async function sleep (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
