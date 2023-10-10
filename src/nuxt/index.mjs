ad3a5eca8f3453347bf9ae9e8732276b68fa7151
André Timermann <andre@timermann.com.br>
2023-10-10 00:03:12 -0300

    nova lib nuxt´

---
+++ src/nuxt/index.mjs
@@ -0,0 +1,32 @@
+/**
+ * **Created on 30/09/2023**
+ *
+ * src/nuxt/index.mjs
+ * @author André Timermann <andre@timermann.com.br>
+ *
+ */
+
+import snakeCase from 'lodash/snakeCase.js'
+
+// initialize RunTimeConfig (Only works with nuxt 3)
+const config = useRuntimeConfig()
+
+/**
+ * Loads a public attribute defined with runtimeconfig and defined with an environment variable, validating whether it was defined
+ *
+ * @param {string} attributeName  Nome do atributo a ser carregado e validado
+ * @param {boolean }required      Se é requerido
+ * @returns {Promise<void>}
+ */
+export function getEnvConfig (attributeName, required = true) {
+
+  const envConfig = config.public[attributeName]
+
+  if (!envConfig && required) {
+    throw showError({
+      statusCode: 500,
+      statusMessage: `Environment variable "NUXT_PUBLIC_${snakeCase(attributeName).toUpperCase()}" not defined in env file.`
+    })
+  }
+  return envConfig
+}
