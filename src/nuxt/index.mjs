b9470a039784503851a74828c5b011689d264cc8
André Timermann <andre@timermann.com.br>
2023-10-13 00:52:48 -0300

    Correção getEnvConfig

--- src/nuxt/index.mjs
+++ src/nuxt/index.mjs
@@ -4,22 +4,20 @@
  * src/nuxt/index.mjs
  * @author André Timermann <andre@timermann.com.br>
  *
+ *  Only works with nuxt 3
  */
 
 import snakeCase from 'lodash/snakeCase.js'
 
-// initialize RunTimeConfig (Only works with nuxt 3)
-// eslint-disable-next-line no-undef
-const config = useRuntimeConfig()
-
 /**
  * Loads a public attribute defined with runtimeconfig and defined with an environment variable, validating whether it was defined
  *
+ * @param {any} config
  * @param {string} attributeName  Nome do atributo a ser carregado e validado
  * @param {boolean }required      Se é requerido
  * @returns {Promise<void>}
  */
-export function getEnvConfig (attributeName, required = true) {
+export function getEnvConfig (config, attributeName, required = true) {
   const envConfig = config.public[attributeName]
 
   if (!envConfig && required) {
